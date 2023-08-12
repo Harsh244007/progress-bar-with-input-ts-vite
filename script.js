512b93042db18217c35d47f81065e381c5914f20
Harsh Patel <Patelharsh241999@gmail.com>
2023-08-12 14:08:50 +0530

    * First commit . .

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
-const numberInput = document.getElementById('numberInput');
-const progressBar = document.getElementById('slider');
+const numberInput = document.getElementById("numberInput");
+const progressBar = document.getElementById("slider");
+const progressBarText = document.getElementById("slider-text");
 
 function fillProgressBar(percentage) {
-  progressBar.style.value = `${percentage}%`;
-  progressBar.value = `${percentage}%`;
+  console.log(percentage);
+  progressBar.value = `${percentage}`;
 }
+(function () {
+  progressBarText.innerText = progressBar.value;
+})();
 
-function startFilling(progress) {
-  if (progress <= 100) {
+progressBar.addEventListener("input", () => {
+  progressBarText.innerText = progressBar.value;
+});
+function startFilling(progress, value) {
+  if (progress <= value) {
+    progressBarText.innerText = progress;
     fillProgressBar(progress);
-    setTimeout(() => startFilling(progress + 2), 2000);
+    setTimeout(() => startFilling(progress + 2, value), 2000);
   }
 }
 
-numberInput.addEventListener('input', () => {
+numberInput.addEventListener("input", () => {
   let value = parseInt(numberInput.value, 10);
   if (value % 2 !== 0) {
     value += 1;
   }
-  value = Math.min(value, 100); // Ensure the value doesn't exceed 100
-  fillProgressBar(0);
-  setTimeout(() => startFilling(2), 2000);
+  value = Math.min(value, 100);
+  progressBar.max = `${value}`;
+  progressBar.value = `0`;
+  progressBarText.innerText = 0;
+  setTimeout(() => startFilling(2, value), 2000);
 });
